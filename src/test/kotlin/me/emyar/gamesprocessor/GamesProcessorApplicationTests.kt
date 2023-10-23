876c91f3e24f15243a910d509d9d91e4b89c2bc5
Yaroslav Emelyanov <emel.yar95@gmail.com>
2023-10-23 18:57:40 +0400

    init

---
+++ src/test/kotlin/me/emyar/gamesprocessor/GamesProcessorApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.emyar.gamesprocessor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GamesProcessorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
