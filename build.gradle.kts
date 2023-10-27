9952a6f0ca5d684b14ff31c220ddf448ee4f9872
Yaroslav Emelyanov <emel.yar95@gmail.com>
2023-10-27 17:36:25 +0400

    Db connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,31 +30,18 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-//	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
 	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
-	implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-money:$exposedVersion")
 	implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
+
+	implementation("com.h2database:h2:2.2.224")
 
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
-
-//tasks.withType<KotlinCompile> {
-//	kotlinOptions {
-//		freeCompilerArgs += "-Xjsr305=strict"
-//		jvmTarget = "17"
-//	}
-//}
 
 kotlin {
 	jvmToolchain(17)
